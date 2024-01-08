544f41dac531c22c3ea150a11102f0f7c3f03ab8
alalonks <atorresrbc6@gmail.com>
2024-01-08 14:48:31 -0500

    finishing up testing

--- FileLoaders.py
+++ FileLoaders.py
@@ -1,4 +1,5 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
+
 
 class FileLoader:
     spark=None
@@ -9,34 +10,48 @@ class FileLoader:
     
     def __init__(self, spark_session, **kwargs):
         
-        self._process_args(self,spark_session, kwargs)
+        self._process_arg(spark_session, kwargs)
     
     def _handleException(func):
         def wrapper(*args):
             try:
                 func(*args)
-            except KeyError as e:
-                print('Key Does Not Exist! ' + e)
-            except Exception:
-                print('There was some sort of error!')
+            except KeyError as ke:
+                print('Key Does Not Exist! ')
+                print(ke)
+            except Exception as e:
+                print('There was some sort of error!') 
+                print(e)
         return wrapper
     
     
     @_handleException
     def _process_arg(self, spark_session, dict_of_args):
         if spark_session != None:
-            spark = spark_session
+            self.spark = spark_session
+            print("Using provide spark_session")
         else:
-            spark = SparkSession.builder.appName("myApp").getOrCreate()
+            self.spark = SparkSession.builder.appName("myApp").getOrCreate()
+            print("Using self generated spark_session")
+        if bool(dict_of_args): 
+            self.file_type =  dict_of_args['dict_of_args']
+            self.infer_schema = dict_of_args['infer_schema']
+            self.first_row_is_header = dict_of_args['first_row_is_header'] 
+            self.delimiter = dict_of_args['delimiter']
         
-        self.file_type =  dict_of_args['dict_of_args']
-        self.infer_schema = dict_of_args['infer_schema']
-        self.first_row_is_header = dict_of_args['first_row_is_header'] 
-        self.delimiter = dict_of_args['delimiter']
 
-    @staticmethod
-    def spark_load(uri_location):
-        df = FileLoader.spark.read.format("lala")
-        return df
+    def spark_load(this, uri_location):
+        print("file_type is " + FileLoader.file_type)
+        print("infer_schema is  " + FileLoader.infer_schema)
+        print("first_row_is_header is " + FileLoader.first_row_is_header)
+        print("delimiter is " + FileLoader.delimiter)
+        
+        df = this.spark.read.format(this.file_type) \
+            .option("inferSchema", this.infer_schema) \
+                .option("header", this.first_row_is_header) \
+                    .option("sep", this.delimiter) \
+                        .load(uri_location)
+        
+        return df 
         
 
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import sys
 from random import random
 from operator import add
+from FileLoaders import FileLoader
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
+
 
 
 if __name__ == "__main__":
@@ -20,8 +22,18 @@ if __name__ == "__main__":
     def f(_: int) -> float:
         x = random() * 2 - 1
         y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+        # print ("x is %f" % x)
+        # print ("y is %f" % y)
+        retVal = 1 if x ** 2 + y ** 2 <= 1 else 0
+        
+        # print ("retVal is %f" % retVal)
+        return  retVal
 
+    fileloader = FileLoader(spark)
+    df = fileloader.spark_load("./data/Pioneers_Dynamo.csv")
+    
+    df.show(10)
+     
     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
     print("Pi is roughly %f" % (4.0 * count / n))
 
